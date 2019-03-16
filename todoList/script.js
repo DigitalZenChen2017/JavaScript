c8a66f2ac918c42629ca84907e4c0635f0050ed1
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-03-15 22:31:14 -0400

    todoList App V8

---
+++ todoList/script.js
@@ -0,0 +1,79 @@
+// set variable todoList equal to an object with a property that stores the todos array
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    // if this.todos.length is equal to 0
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        // check if .completed is true
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function(todoText) { // addTodo('hi')
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    // this.todos[position] = newValue;
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed; // bang operator ! reverses boolean value opposite
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    // Get number of completed todos.
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    // Case 1: If eeverything’s true, make everything false.
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    // Case 2: Otherwise, make everything true.
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    } 
+    this.displayTodos();
+  }
+};
+
+var displayTodosbutton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();  
+});
+
+
+
+
