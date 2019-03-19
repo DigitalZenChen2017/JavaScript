fb41d66f3b782129b9c41ccfa7452c4b850e9af6
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-03-19 14:12:17 -0400

    todoList V9

--- todoList/script.js
+++ todoList/script.js
@@ -1,42 +1,22 @@
 // set variable todoList equal to an object with a property that stores the todos array
 var todoList = {
   todos: [],
-  displayTodos: function() {
-    // if this.todos.length is equal to 0
-    if (this.todos.length === 0) {
-      console.log('Your todo list is empty!');
-    } else {
-      console.log('My Todos:');
-      for (var i = 0; i < this.todos.length; i++) {
-        // check if .completed is true
-        if (this.todos[i].completed === true) {
-          console.log('(x)', this.todos[i].todoText);
-        } else {
-          console.log('( )', this.todos[i].todoText);
-        }
-      }
-    }
-  },
   addTodo: function(todoText) { // addTodo('hi')
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   changeTodo: function(position, todoText) {
     // this.todos[position] = newValue;
     this.todos[position].todoText = todoText;
-    this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed; // bang operator ! reverses boolean value opposite
-    this.displayTodos();
   },
   toggleAll: function() {
     var totalTodos = this.todos.length;
@@ -59,21 +39,16 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-    this.displayTodos();
   }
 };
 
+// Provide Button Handlers for UI
 var handlers = {
-  displayTodos: function() {
-    todoList.displayTodos();
-  },
-  toggleAll: function() {
-    todoList.toggleAll();
-  },
   addTodo: function() {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value); // DOM allows us to obtain variable's value
     addTodoTextInput.value = '';
+    view.displayTodos();
   },
   changeTodo: function() {
     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput')
@@ -81,6 +56,44 @@ var handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
+    view.displayTodos();
+  },
+  deleteTodo: function() {
+    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+    deleteTodoPositionInput.value = '';
+    view.displayTodos();
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = '';
+    view.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+    view.displayTodos();
   }
-  
+};
+
+// Show todoList to HTML Page
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement('li');
+      var todo = todoList.todos[i];
+      var todoTextWithCompletion = '';
+
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(x) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText;
+      }
+
+      todoLi.textContent = todoTextWithCompletion;
+      todosUl.appendChild(todoLi);
+    }
+  }
 };
