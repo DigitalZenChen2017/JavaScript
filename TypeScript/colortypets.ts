77df0df7f42dccb6070fe5703c66ac3dae7846f2
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-03-28 15:18:59 -0400

    3/28/2019 Practice

---
+++ TypeScript/colortypets.ts
@@ -0,0 +1,4 @@
+type ColorType = [string, number, number, number];
+let red: ColorType = ['Red', 1, 0, 0];
+let green: ColorType = ['Green', 0, 1, 0];
+let violet: [string number, number, number] = ['Violet', 1, 0, 1];
