d719f58e0eb24e006941b85f620964159cb7cd25
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-04-17 01:12:09 -0400

    AngularJS

---
+++ AngularJS/my-cv/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Boris Chen\'s CV';
+  contactName = 'Boris Chen';
+  contactPhoneNumber = 5132909828;
+  contactEmail = 'theboris.chen@gmail.com';
+}
