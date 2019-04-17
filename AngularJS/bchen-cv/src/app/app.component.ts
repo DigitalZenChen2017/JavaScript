2343ccc5ce86bf94076162ce1be470a13ff7dee6
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-04-17 18:55:20 -0400

    04172019 Update

---
+++ AngularJS/bchen-cv/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  firstName = 'Boris';
+  lastName = 'Chen';
+  name = 'Boris Chen';
+  phoneNumber = 5132909828;
+  email = 'theboris.chen@gmail.com';
+}
