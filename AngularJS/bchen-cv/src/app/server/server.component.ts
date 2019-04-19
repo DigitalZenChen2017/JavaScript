19edab3f363188f07775f66cecb58c84806773ad
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-04-19 11:04:00 -0400

    Added AngularJS Files

--- AngularJS/bchen-cv/src/app/server/server.component.ts
+++ AngularJS/bchen-cv/src/app/server/server.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
-    selector: 'app-server', // prefix with "app" followed by name
-    templateUrl: './server.component.html'
+  selector: 'app-server', // prefix with "app" followed by name
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- AngularJS/bchen-cv/src/app/servers/servers.component.ts
+++ AngularJS/bchen-cv/src/app/servers/servers.component.ts
@@ -5,9 +5,10 @@ import { Server } from 'https';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server><app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server><app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
