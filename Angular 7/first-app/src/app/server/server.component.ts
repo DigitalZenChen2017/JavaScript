4946800e2736c022a1a6961f3dd8916007f0f46f
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-05-21 18:17:05 -0400

    05212019

--- Angular 7/first-app/src/app/server/server.component.ts
+++ Angular 7/first-app/src/app/server/server.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-selector: 'app-server',
-templateUrl: './server.component.html',
+  selector: 'app-server',
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
+// tslint:disable-next-line: no-inferrable-types
+  serverId: number = 10;
+// tslint:disable-next-line: no-inferrable-types
+  serverStatus: string = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- Angular 7/first-app/src/app/servers/servers.component.ts
+++ Angular 7/first-app/src/app/servers/servers.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-// tslint:disable-next-line: component-selector
+  // tslint:disable-next-line: component-selector
   selector: 'app-servers',
   /* templateUrl: './servers.component.html',*/
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `, /*template is REQUIRED!*/
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `, /*template is REQUIRED!*/
+  templateUrl: './servers.component.html'
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created';
+  serverName = 'Test Server';
+  userName = 'Enter UserName';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server Was Created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
 }
